a89ad5459f257ca877b1c2063ca4c9a653438e4c
twlghtzn <orsolya.bor@gmail.com>
2020-07-16 20:50:04 +0200

    Adds TwoNumbers exercise

---
+++ week_01/day_4/TwoNumbers.java
@@ -0,0 +1,15 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 22;
+
+        System.out.println(13 + 22);
+        System.out.println(22 - 13);
+        System.out.println(22 * 13);
+        System.out.println(22. / 13.);
+        System.out.println(22 / 13);
+        System.out.println(22 % 13);
+
+    }
+}
