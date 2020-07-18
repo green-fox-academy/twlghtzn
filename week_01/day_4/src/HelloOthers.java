de83b742760f9b2cbb453cdfd6e3625a59b1778f
twlghtzn <orsolya.bor@gmail.com>
2020-07-18 11:27:49 +0200

    Empties repository

--- week_01/day_4/src/HelloOthers.java
+++
@@ -1,8 +0,0 @@
-public class HelloOthers {
-    public static void main(String[] args) {
-        System.out.println("Hello Márk!");
-        System.out.println("Hello Dani!");
-        System.out.println("Hello Aurél!");
-
-    }
-}
--- week_01/day_4/src/IntroduceYourself.java
+++
@@ -1,7 +0,0 @@
-public class IntroduceYourself {
-    public static void main(String[] args) {
-        System.out.println("Bor Orsolya");
-        System.out.println(45);
-        System.out.println(1.75);
-    }
-}
