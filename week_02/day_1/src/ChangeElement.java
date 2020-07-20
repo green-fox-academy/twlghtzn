c23a5b86429ef8a903204e58b3341e5824a42e83
twlghtzn <orsolya.bor@gmail.com>
2020-07-20 16:10:32 +0200

    Adds ChangeElement exercise

---
+++ week_02/day_1/src/ChangeElement.java
@@ -0,0 +1,14 @@
+public class ChangeElement {
+  public static void main(String[] args) {
+    // Ready
+    // - Create an array variable named `numbers`
+    //   with the following content: `[1, 2, 3, 8, 5, 6]`
+    // - Change the 8 to 4
+    // - Print the fourth element
+
+    int[] numbers = {1, 2, 3, 8, 5, 6};
+    numbers[3] = 4;
+    System.out.println("The fourth element is now: " + numbers[3]);
+
+  }
+}
