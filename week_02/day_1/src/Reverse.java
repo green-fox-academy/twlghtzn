6f042a10156b2937340de7ebc671c4f6ff2def41
twlghtzn <orsolya.bor@gmail.com>
2020-07-20 16:14:21 +0200

    Adds Reverse exercise

---
+++ week_02/day_1/src/Reverse.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Reverse {
+  public static void main(String[] args) {
+    // - Create an array variable named `numbers`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Reverse the order of the elements in `numbers`
+    // - Print the elements of the reversed `numbers`
+
+    int[] numbers = {3, 4, 5, 6, 7};
+    int[] numbersReversed = {0, 0, 0, 0, 0};
+
+    for (int i = 0; i < numbersReversed.length; i++) {
+      numbersReversed[i] = numbers[numbers.length - 1 - i];
+    }
+    System.out.println(Arrays.toString(numbersReversed));
+  }
+}
