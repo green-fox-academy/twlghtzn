a2577c120e9af9e48d1e6e28c4b559da72d77b15
twlghtzn <orsolya.bor@gmail.com>
2020-07-20 16:11:52 +0200

    Adds CompareLength exercise

---
+++ week_02/day_1/src/CompareLength.java
@@ -0,0 +1,20 @@
+public class CompareLength {
+  public static void main(String[] args) {
+    // Ready
+    // - Create an array variable named `firstArrayOfNumbers`
+    //   with the following content: `[1, 2, 3]`
+    // - Create an array variable named `secondArrayOfNumbers`
+    //   with the following content: `[4, 5]`
+    // - Print "secondArrayOfNumbers is longer" if `secondArrayOfNumbers` has more
+    //   elements than `firstArrayOfNumbers`
+
+    int[] firstArrayOfNumbers = {1, 2, 3};
+    int[] secondArrayOfNumbers = {4, 5};
+
+    if (secondArrayOfNumbers.length > firstArrayOfNumbers.length) {
+      System.out.println("secondArrayOfNumbers is longer");
+    } else {
+      System.out.println("secondArrayOfNumbers is not longer");
+    }
+  }
+}
