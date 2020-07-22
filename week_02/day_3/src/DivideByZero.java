6062063a1dfb6d2b783862b943cae00d1c65e7ca
twlghtzn <orsolya.bor@gmail.com>
2020-07-22 15:55:01 +0200

    Adds DivideByZero exercise

---
+++ week_02/day_3/src/DivideByZero.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class DivideByZero {
+  public static void main(String[] args) {
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0
+    int divider = 0;
+    try {
+      System.out.println(divideTen(divider));
+    } catch (RuntimeException e) {
+      System.out.println("fail");
+    }
+  }
+  public static float divideTen(int a) {
+    float result = 10 / a;
+    return result;
+  }
+}
