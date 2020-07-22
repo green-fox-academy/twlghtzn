2b626199357c1301521462905eb40ca5b21984c0
twlghtzn <orsolya.bor@gmail.com>
2020-07-22 15:56:14 +0200

    Adds WriteMultipleLines exercise

---
+++ week_02/day_3/src/WriteMultipleLines.java
@@ -0,0 +1,34 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+
+public class WriteMultipleLines {
+  public static void main(String[] args) {
+    // Create a function that takes 3 parameters: a path, a word and a number
+    // and is able to write into a file.
+    // The path parameter should be a string that describes the location of the file you wish to modify
+    // The word parameter should also be a string that will be written to the file as individual lines
+    // The number parameter should describe how many lines the file should have.
+    // If the word is 'apple' and the number is 5, it should write 5 lines
+    // into the file and each line should read 'apple'
+    // The function should not raise any errors if it could not write the file.
+    String path = "C:\\Users\\gyors\\Documents\\twlghtzn\\week_02\\day_3\\my-file.txt";
+    String word = "apple";
+    int number = 3;
+    fileManipulator(path, word, number);
+  }
+  public static void fileManipulator(String a, String b, int c) {
+    try {
+      Path path = Paths.get(a);
+      String bToNewLine = b + "\n";
+      byte[] bToBytes = bToNewLine.getBytes();
+      Files.write(path, bToBytes);
+      for (int i = 1; i < c; i++) {
+        Files.write(path, bToBytes, StandardOpenOption.APPEND);
+      }
+    } catch (Exception ignored) {
+    }
+  }
+}
