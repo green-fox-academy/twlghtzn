c0edd63530ab6c3e10dfb30ce5ae3f1b3b92fb8f
twlghtzn <orsolya.bor@gmail.com>
2020-07-28 15:42:30 +0200

    Adds SharpieSet exercise

---
+++ week_03/day_2/src/sharpieset/Sharpie.java
@@ -0,0 +1,16 @@
+package sharpieset;
+
+public class Sharpie {
+  String name;
+  String color;
+  float width;
+  float inkAmount;
+
+  public Sharpie(String color, float width) {
+    this.name = color;
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 30;
+  }
+
+}
