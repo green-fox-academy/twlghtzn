f110fc3e38149bf112da4b49a647b8314058e190
twlghtzn <orsolya.bor@gmail.com>
2020-07-29 20:59:16 +0200

    Adds NumberAdder exercise

---
+++ week_03/day_3/src/numberadder/NumberAdder.java
@@ -0,0 +1,20 @@
+package numberadder;
+
+public class NumberAdder {
+  // Write a recursive function that takes one parameter:
+  // n and adds numbers from 1 to n.
+  public static void main(String[] args) {
+
+    System.out.println(numberAdder(55));
+  }
+  public static int numberAdder(int n) {
+    int sum = n;
+    if ((n - 1) > 0) {
+      sum += numberAdder(n - 1);
+      return sum;
+    } else {
+      return sum;
+    }
+  }
+
+}
