029f4c5e3dd1f9ca1c8dffa149b04759c316b04c
twlghtzn <orsolya.bor@gmail.com>
2020-07-30 22:56:26 +0200

    Adds CountLetters exercise and test

---
+++ week_03/day_4/src/countletters/CountLetters.java
@@ -0,0 +1,31 @@
+package countletters;
+
+import java.util.HashMap;
+
+public class CountLetters {
+
+  public static void main(String[] args) {
+
+    String b = "Ezkésztéboly";
+    String a = "AAaaBBbbCCcc12345";
+
+
+    HashMap dictionary = countLetters(a);
+    System.out.println(dictionary.keySet().toString());
+    System.out.println(dictionary.values().toString());
+  }
+  public static HashMap countLetters(String a) {
+    HashMap dictionary = new HashMap();
+    for (int i = 0; i < a.length(); i++) {
+      if (!dictionary.containsKey(a.charAt(i))) {
+        dictionary.put(a.charAt(i), 1);
+      } else {
+        dictionary.put(a.charAt(i), (int) dictionary.get(a.charAt(i)) + 1);
+      }
+    }
+    return dictionary;
+  }
+
+
+
+}
---
+++ week_03/day_4/tests/countletters/CountLettersTest.java
@@ -0,0 +1,41 @@
+package countletters;
+
+import static countletters.CountLetters.countLetters;
+import static org.junit.Assert.assertTrue;
+
+
+import java.util.HashMap;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CountLettersTest {
+  CountLetters countLetters;
+
+  @Before
+  public void setup() {
+    countLetters = new CountLetters();
+  }
+
+  @Test
+  public void isCountLettersCorrect() {
+    String a = "AAaaBBbbCCcc12345";
+    HashMap test = new HashMap();
+    test.put('A', 2);
+    test.put('a', 2);
+    test.put('1', 1);
+    test.put('B', 2);
+    test.put('b', 2);
+    test.put('2', 1);
+    test.put('C', 2);
+    test.put('c', 2);
+    test.put('3', 1);
+    test.put('4', 1);
+    test.put('5', 1);
+    assertTrue(test.equals(countLetters(a)));
+
+  }
+
+
+
+
+}
