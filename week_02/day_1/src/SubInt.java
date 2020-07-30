396f08c57945302dfe49158073134a75b01d5062
twlghtzn <orsolya.bor@gmail.com>
2020-07-30 22:54:22 +0200

    Corrects SubInt exercise returns

--- week_02/day_1/src/SubInt.java
+++ week_02/day_1/src/SubInt.java
@@ -6,23 +6,30 @@ public class SubInt {
     //  Returns the indices of the integers in the array of which the first number is a part of
     //  Or returns an empty array if the number is not part of any of the integers in the array
 
-    int a = 6;
-    int[] anArray = {2, 3, 4, 5};
+    int a = 2;
+    int[] anArray = {21, 33, 42, 5, 67, 83, 24};
     int[] returnArray = searchTheArray(a, anArray);
     System.out.println(Arrays.toString(returnArray));
   }
 
   public static int[] searchTheArray(int a, int[] anArray) {
-    int[] indicesArray = new int[anArray.length];
-    int[] emptyArray = new int[0];
+    String b = Integer.toString(a);
+    int counter = 0;
+    int[] c = new int[0];
     for (int i = 0; i < anArray.length; i++) {
-      indicesArray[i] = i;
-    }
-    for (int value : anArray) {
-      if (value == a) {
-        return indicesArray;
+      if (Integer.toString(anArray[i]).contains(b)) {
+        counter++;
       }
     }
-    return emptyArray;
+    c = new int[counter];
+    int indicesCounter = 0;
+    for (int i = 0; i < anArray.length; i++) {
+        if (Integer.toString(anArray[i]).contains(b)) {
+          c[indicesCounter] = i;
+          indicesCounter++;
+        }
+      }
+    return c;
   }
 }
+
