3895fc6d41244522d4abfddca971544322e29d0b
twlghtzn <orsolya.bor@gmail.com>
2020-08-05 11:13:00 +0200

    Adds Cloneable exercise

---
+++ week_04/day_3/src/cloneable/Main.java
@@ -0,0 +1,9 @@
+package cloneable;
+
+public class Main {
+  public static void main(String[] args) {
+    Student john = new Student("John", 20, "male", "BME");
+    Student john2 = john.clone();
+    john2.introduce();
+  }
+}
---
+++ week_04/day_3/src/cloneable/Student.java
@@ -0,0 +1,39 @@
+package cloneable;
+
+public class Student extends Person{
+  String previousOrganization;
+  int skippedDays;
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  public Student() {
+    super();
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.name + ", a " + this.age + " year old " + this.gender + " from " + this.previousOrganization + " who skipped " + this.skippedDays + " days from the course already.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    this.skippedDays += numberOfDays;
+  }
+
+  @Override
+  public Student clone() {
+    String cloneName = this.name.toLowerCase() + "TheClone";
+    Student clone = new Student(cloneName, age, gender, previousOrganization);
+    return clone;
+  }
+}
